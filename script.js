2c0702a66674e222c9c9319bfe77592462186a50
Dave Luke Eichenberger <davelukeeichenberger@outlook.com>
2021-09-07 15:22:54 +0200

    fix

---
+++ script.js
@@ -0,0 +1,6 @@
+// Navbar Hover Shadow
+$(".navbar-hover").hover( function() {  
+    $(this).css("text-shadow", "0 0 20px currentColor");
+}, function() {
+    $(this).css("text-shadow", "none");
+});
